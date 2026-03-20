d2125d15d85c74583cdd4d5e8b73c370f22a6004
Mishkatul Habib <mhkabbo314@gmail.com>
2026-03-20 18:48:08 +0600

    Frog2

---
+++ AtCoder Educational DP/Frog2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1e9 + 7;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // If the stone is at the i-th pos
+    // it can go either in (i+1) or (i+2)th pos
+    // we have to minimize the cost
+
+
+    int N, K;
+    cin >> N >> K;
+    
+    vector<int> h(N);
+    for(int &x : h) cin >> x;
+
+    vector<int> dp(N+1, INF); // dp[i] will store the min cost to land on i
+    // Base cases
+    dp[1] = 0;
+    dp[2] = abs(h[0] - h[1]);
+    for(int i = 3; i <= N; i++){
+        // currently i am on i 
+        // i can come to i from i-1, i-2, i - k + 1 ...
+        for(int j = 1; j <= K; j++){
+            if(i > j) dp[i] = min(dp[i-j] + abs(h[i-j-1] - h[i-1]), dp[i]);
+            else break;
+        }
+    }
+
+    cout << dp[N] << endl;
+
+    return 0;
+}
